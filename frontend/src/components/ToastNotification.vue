8dc7e757c6e7ff536ed58c08fec03fffd7c59e02
Robert <54269637+R-udren@users.noreply.github.com>
2025-06-01 16:50:29 +0300

    update error icon for toast notification component

--- frontend/src/components/ToastNotification.vue
+++ frontend/src/components/ToastNotification.vue
@@ -44,7 +44,7 @@ const typeConfig = {
   error: {
     bg: 'from-red-500/80 to-red-600/80',
     border: 'border-red-400/30',
-    icon: 'M6 18L18 6M6 6l12 12',
+    icon: 'M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z',
   },
   info: {
     bg: 'from-primary-500/80 to-primary-600/80',
